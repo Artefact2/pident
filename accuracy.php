b62920c071e615cb7aad0a8448af8c57aad745b9
Artefact2 <artefact2@gmail.com>
2011-07-15 18:43:19 +0200

    Better wording, show overall accuracy.

--- accuracy.php
+++ accuracy.php
@@ -16,18 +16,6 @@ if(!$success) {
 	die('Accuracy data is not available. FUUUUUUUUU-');
 }
 
-echo "<!DOCTYPE html>
-<html>
-<head>
-".HEADER."
-<title>Score accuracy</title>
-</head>
-<body>
-<h1>Score accuracy</h1>
-<p id='back'><a href='/'>&larr; Back to the main page</a></p>
-<p class='notice'>The results below may be biased, since the tests was only done on blocks found by their respective pool. These testing conditions do not represent exactly the <em>unknown block</em> scenario.</p>
-";
-
 $cols = "<tr>
 <th rowspan='2'>&#9650; Pool</th>
 <th colspan='5' title='Blocks found by the pool, with scores successfully indicating it was found by this pool.'><span>Accuracy</span></th>
@@ -35,11 +23,14 @@ $cols = "<tr>
 </tr>
 <tr>
 <th>Most likely</th>
-<th>Probably</th>
-<th>Wild guess</th>
-<th>Not sure at all</th>
+<th>Probably<br /><small>(or better)</small></th>
+<th>Wild guess<br /><small>(or better)</small></th>
+<th>Not sure at all<br /><small>(or better)</small></th>
 <th title='Blocks found by the pool, but with scores indicating it was found by another pool.'><span>False positives</span></th>
 </tr>";
+
+$overallTotal = 0;
+$overallCoeff = 0;
 
 $rows = '';
 ksort($accuracy);
@@ -47,34 +38,54 @@ foreach($accuracy as $pool => $a) {
 	$rows .= "<tr>\n";
 	$rows .= "<td>".prettyPool($pool)."</td>\n";
 
-	$p = @round(100 * (
+	$sSize = $a['sample_size'];
+
+	$p = @(100 * (
 		$a['accuracy'][C_MOST_LIKELY]
 	) / $a['sample_size']);
-	$rows .= "<td style='background-color: rgba(166, 230, 132, ".number_format($p / 100, 2).");'>$p %</td>\n";
+	$rows .= "<td style='background-color: rgba(166, 230, 132, ".number_format($p / 100, 2).");'>".round($p)." %</td>\n";
+	$overallTotal += $p * 6 * $sSize;
 
-	$p = @round(100 * (
+	$p = @(100 * (
 		$a['accuracy'][C_MOST_LIKELY] + $a['accuracy'][C_PROBABLY]
 	) / $a['sample_size']);
-	$rows .= "<td style='background-color: rgba(176, 230, 100, ".number_format($p / 100, 2).");'>$p %</td>\n";
+	$rows .= "<td style='background-color: rgba(176, 230, 100, ".number_format($p / 100, 2).");'>".round($p)." %</td>\n";
+	$overallTotal += $p * 4 * $sSize;
 
-	$p = @round(100 * (
+	$p = @(100 * (
 		$a['accuracy'][C_MOST_LIKELY] + $a['accuracy'][C_PROBABLY] + $a['accuracy'][C_WILD_GUESS]
 	) / $a['sample_size']);
-	$rows .= "<td style='background-color: rgba(186, 230, 70, ".number_format($p / 100, 2).");'>$p %</td>\n";
+	$rows .= "<td style='background-color: rgba(186, 230, 70, ".number_format($p / 100, 2).");'>".round($p)." %</td>\n";
+	$overallTotal += $p * 2 * $sSize;
 
-	$p = @round(100 * (
+	$p = @(100 * (
 		$a['accuracy'][C_MOST_LIKELY] + $a['accuracy'][C_PROBABLY] + $a['accuracy'][C_WILD_GUESS] + $a['accuracy'][C_NOT_SURE_AT_ALL]
 	) / $a['sample_size']);
-	$rows .= "<td style='background-color: rgba(196, 230, 60, ".number_format($p / 100, 2).");'>$p %</td>\n";
+	$rows .= "<td style='background-color: rgba(196, 230, 60, ".number_format($p / 100, 2).");'>".round($p)." %</td>\n";
+	$overallTotal += $p * $sSize;
 
 	$p = @round(100 * $a['false_positives'] / $a['sample_size']);
 	$rows .= "<td style='background-color: rgba(255, 80, 80, ".number_format($p / 100, 2).");'>$p %</td>\n";
 
-	$rows .= "<td>".$a['sample_size']."</td>\n";
+	$rows .= "<td>".$sSize."</td>\n";
 	$rows .= "</tr>\n";
+
+	$overallCoeff += 13 * $sSize;
 }
 
-echo "<table id='accuracy'>
+$overall = round($overallTotal / $overallCoeff, 2);
+
+echo "<!DOCTYPE html>
+<html>
+<head>
+".HEADER."
+<title>Score accuracy</title>
+</head>
+<body>
+<h1>Score accuracy <small>(<span title='Weighted average of invividual pool accuracies (weighted by sample size).'>overall accuracy: $overall %</span>)</small></h1>
+<p id='back'><a href='/'>&larr; Back to the main page</a></p>
+<p class='notice'>The results below may be biased, since the tests were only done on blocks found by their respective pool. These testing conditions do not represent exactly the <em>unknown block</em> scenario.</p>
+<table id='accuracy'>
 <thead>
 $cols
 </thead>

574d20764a891ecb0e415e82a448fb316113283c
Artefact2 <artefact2@gmail.com>
2011-07-29 15:40:13 +0200

    Update schema.

--- schema_postgre.sql
+++ schema_postgre.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.0.4
 -- Dumped by pg_dump version 9.0.4
--- Started on 2011-07-17 01:49:48 CEST
+-- Started on 2011-07-29 15:40:00 CEST
 
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
@@ -14,7 +14,7 @@ SET client_min_messages = warning;
 SET escape_string_warning = off;
 
 --
--- TOC entry 327 (class 2612 OID 11574)
+-- TOC entry 331 (class 2612 OID 11574)
 -- Name: plpgsql; Type: PROCEDURAL LANGUAGE; Schema: -; Owner: -
 --
 
@@ -26,7 +26,7 @@ SET search_path = public, pg_catalog;
 SET default_with_oids = false;
 
 --
--- TOC entry 1519 (class 1259 OID 20086)
+-- TOC entry 1523 (class 1259 OID 20086)
 -- Dependencies: 6
 -- Name: blocks; Type: TABLE; Schema: public; Owner: -
 --
@@ -43,7 +43,7 @@ CREATE TABLE blocks (
 
 
 --
--- TOC entry 1525 (class 1259 OID 20678)
+-- TOC entry 1529 (class 1259 OID 20678)
 -- Dependencies: 6
 -- Name: blocks_transactions; Type: TABLE; Schema: public; Owner: -
 --
@@ -55,7 +55,7 @@ CREATE TABLE blocks_transactions (
 
 
 --
--- TOC entry 1521 (class 1259 OID 20092)
+-- TOC entry 1525 (class 1259 OID 20092)
 -- Dependencies: 6
 -- Name: tx_in; Type: TABLE; Schema: public; Owner: -
 --
@@ -69,8 +69,8 @@ CREATE TABLE tx_in (
 
 
 --
--- TOC entry 1522 (class 1259 OID 20095)
--- Dependencies: 1810 6
+-- TOC entry 1526 (class 1259 OID 20095)
+-- Dependencies: 1818 6
 -- Name: tx_out; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -85,8 +85,8 @@ CREATE TABLE tx_out (
 
 
 --
--- TOC entry 1526 (class 1259 OID 21880)
--- Dependencies: 1615 6
+-- TOC entry 1530 (class 1259 OID 21880)
+-- Dependencies: 1621 6
 -- Name: address_from; Type: VIEW; Schema: public; Owner: -
 --
 
@@ -95,8 +95,8 @@ CREATE VIEW address_from AS
 
 
 --
--- TOC entry 1527 (class 1259 OID 21885)
--- Dependencies: 1616 6
+-- TOC entry 1531 (class 1259 OID 21885)
+-- Dependencies: 1622 6
 -- Name: address_to; Type: VIEW; Schema: public; Owner: -
 --
 
@@ -105,7 +105,30 @@ CREATE VIEW address_to AS
 
 
 --
--- TOC entry 1523 (class 1259 OID 20121)
+-- TOC entry 1524 (class 1259 OID 20089)
+-- Dependencies: 1817 6
+-- Name: transactions; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE transactions (
+    transaction_id bit(256) NOT NULL,
+    size integer NOT NULL,
+    is_generation boolean DEFAULT false NOT NULL
+);
+
+
+--
+-- TOC entry 1534 (class 1259 OID 32774)
+-- Dependencies: 1625 6
+-- Name: blocks_generated; Type: VIEW; Schema: public; Owner: -
+--
+
+CREATE VIEW blocks_generated AS
+    SELECT sum(tx_out.amount) AS generated, blocks.number FROM (((tx_out JOIN transactions ON (((transactions.transaction_id = tx_out.transaction_id) AND (transactions.is_generation = true)))) JOIN blocks_transactions ON ((blocks_transactions.transaction_id = tx_out.transaction_id))) JOIN blocks ON ((blocks.hash = blocks_transactions.block))) GROUP BY blocks.number;
+
+
+--
+-- TOC entry 1527 (class 1259 OID 20121)
 -- Dependencies: 6
 -- Name: scores; Type: TABLE; Schema: public; Owner: -
 --
@@ -118,8 +141,8 @@ CREATE TABLE scores (
 
 
 --
--- TOC entry 1528 (class 1259 OID 21905)
--- Dependencies: 1617 6
+-- TOC entry 1532 (class 1259 OID 21905)
+-- Dependencies: 1623 6
 -- Name: scores_blocks; Type: VIEW; Schema: public; Owner: -
 --
 
@@ -128,7 +151,7 @@ CREATE VIEW scores_blocks AS
 
 
 --
--- TOC entry 1524 (class 1259 OID 20128)
+-- TOC entry 1528 (class 1259 OID 20128)
 -- Dependencies: 6
 -- Name: scores_pool_averages; Type: TABLE; Schema: public; Owner: -
 --
@@ -149,21 +172,18 @@ CREATE TABLE scores_pool_averages (
 
 
 --
--- TOC entry 1520 (class 1259 OID 20089)
--- Dependencies: 1809 6
--- Name: transactions; Type: TABLE; Schema: public; Owner: -
+-- TOC entry 1533 (class 1259 OID 32765)
+-- Dependencies: 1624 6
+-- Name: transaction_count; Type: VIEW; Schema: public; Owner: -
 --
 
-CREATE TABLE transactions (
-    transaction_id bit(256) NOT NULL,
-    size integer NOT NULL,
-    is_generation boolean DEFAULT false NOT NULL
-);
+CREATE VIEW transaction_count AS
+    SELECT blocks.hash, count(blocks_transactions.transaction_id) AS count FROM (blocks JOIN blocks_transactions ON ((blocks_transactions.block = blocks.hash))) GROUP BY blocks.hash;
 
 
 --
--- TOC entry 1814 (class 2606 OID 20142)
--- Dependencies: 1519 1519
+-- TOC entry 1822 (class 2606 OID 20142)
+-- Dependencies: 1523 1523
 -- Name: blocks_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -172,8 +192,8 @@ ALTER TABLE ONLY blocks
 
 
 --
--- TOC entry 1833 (class 2606 OID 20682)
--- Dependencies: 1525 1525 1525
+-- TOC entry 1843 (class 2606 OID 20682)
+-- Dependencies: 1529 1529 1529
 -- Name: blocks_transactions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -182,8 +202,8 @@ ALTER TABLE ONLY blocks_transactions
 
 
 --
--- TOC entry 1828 (class 2606 OID 20144)
--- Dependencies: 1523 1523 1523
+-- TOC entry 1838 (class 2606 OID 20144)
+-- Dependencies: 1527 1527 1527
 -- Name: scores_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -192,8 +212,8 @@ ALTER TABLE ONLY scores
 
 
 --
--- TOC entry 1830 (class 2606 OID 20146)
--- Dependencies: 1524 1524
+-- TOC entry 1840 (class 2606 OID 20146)
+-- Dependencies: 1528 1528
 -- Name: scores_pool_avrages_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -202,8 +222,8 @@ ALTER TABLE ONLY scores_pool_averages
 
 
 --
--- TOC entry 1818 (class 2606 OID 20148)
--- Dependencies: 1520 1520
+-- TOC entry 1827 (class 2606 OID 20148)
+-- Dependencies: 1524 1524
 -- Name: transactions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -212,8 +232,8 @@ ALTER TABLE ONLY transactions
 
 
 --
--- TOC entry 1822 (class 2606 OID 20150)
--- Dependencies: 1521 1521 1521
+-- TOC entry 1832 (class 2606 OID 20150)
+-- Dependencies: 1525 1525 1525
 -- Name: tx_in_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -222,8 +242,8 @@ ALTER TABLE ONLY tx_in
 
 
 --
--- TOC entry 1826 (class 2606 OID 20152)
--- Dependencies: 1522 1522 1522
+-- TOC entry 1836 (class 2606 OID 20152)
+-- Dependencies: 1526 1526 1526
 -- Name: tx_out_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -232,8 +252,8 @@ ALTER TABLE ONLY tx_out
 
 
 --
--- TOC entry 1811 (class 1259 OID 20153)
--- Dependencies: 1519
+-- TOC entry 1819 (class 1259 OID 20153)
+-- Dependencies: 1523
 -- Name: blocks_found_by_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -241,8 +261,8 @@ CREATE INDEX blocks_found_by_idx ON blocks USING btree (found_by);
 
 
 --
--- TOC entry 1812 (class 1259 OID 20154)
--- Dependencies: 1519
+-- TOC entry 1820 (class 1259 OID 20154)
+-- Dependencies: 1523
 -- Name: blocks_number_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -250,8 +270,8 @@ CREATE INDEX blocks_number_idx ON blocks USING btree (number);
 
 
 --
--- TOC entry 1815 (class 1259 OID 20155)
--- Dependencies: 1519
+-- TOC entry 1823 (class 1259 OID 20155)
+-- Dependencies: 1523
 -- Name: blocks_time_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -259,8 +279,8 @@ CREATE INDEX blocks_time_idx ON blocks USING btree ("time");
 
 
 --
--- TOC entry 1831 (class 1259 OID 21947)
--- Dependencies: 1525
+-- TOC entry 1841 (class 1259 OID 21947)
+-- Dependencies: 1529
 -- Name: blocks_transactions_block_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -268,8 +288,8 @@ CREATE INDEX blocks_transactions_block_idx ON blocks_transactions USING btree (b
 
 
 --
--- TOC entry 1834 (class 1259 OID 21948)
--- Dependencies: 1525
+-- TOC entry 1844 (class 1259 OID 21948)
+-- Dependencies: 1529
 -- Name: blocks_transactions_transaction_id_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -277,8 +297,8 @@ CREATE INDEX blocks_transactions_transaction_id_idx ON blocks_transactions USING
 
 
 --
--- TOC entry 1816 (class 1259 OID 20156)
--- Dependencies: 1519
+-- TOC entry 1824 (class 1259 OID 20156)
+-- Dependencies: 1523
 -- Name: fki_blocks_previous_hash_fkey; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -286,8 +306,8 @@ CREATE INDEX fki_blocks_previous_hash_fkey ON blocks USING btree (previous_hash)
 
 
 --
--- TOC entry 1819 (class 1259 OID 20157)
--- Dependencies: 1521 1521
+-- TOC entry 1829 (class 1259 OID 20157)
+-- Dependencies: 1525 1525
 -- Name: fki_tx_id_previous_out_fkey; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -295,8 +315,8 @@ CREATE INDEX fki_tx_id_previous_out_fkey ON tx_in USING btree (previous_out, n);
 
 
 --
--- TOC entry 1820 (class 1259 OID 20158)
--- Dependencies: 1521 1521
+-- TOC entry 1830 (class 1259 OID 20158)
+-- Dependencies: 1525 1525
 -- Name: fki_tx_in_previous_out_fkey; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -304,8 +324,26 @@ CREATE INDEX fki_tx_in_previous_out_fkey ON tx_in USING btree (previous_out, pre
 
 
 --
--- TOC entry 1823 (class 1259 OID 20160)
--- Dependencies: 1522
+-- TOC entry 1825 (class 1259 OID 32769)
+-- Dependencies: 1524
+-- Name: transactions_is_generation_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX transactions_is_generation_idx ON transactions USING btree (is_generation);
+
+
+--
+-- TOC entry 1828 (class 1259 OID 32779)
+-- Dependencies: 1524
+-- Name: transactions_size_idx; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX transactions_size_idx ON transactions USING btree (size);
+
+
+--
+-- TOC entry 1833 (class 1259 OID 20160)
+-- Dependencies: 1526
 -- Name: tx_out_address_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -313,8 +351,8 @@ CREATE INDEX tx_out_address_idx ON tx_out USING btree (address);
 
 
 --
--- TOC entry 1824 (class 1259 OID 20161)
--- Dependencies: 1522
+-- TOC entry 1834 (class 1259 OID 20161)
+-- Dependencies: 1526
 -- Name: tx_out_is_payout_idx; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -322,8 +360,8 @@ CREATE INDEX tx_out_is_payout_idx ON tx_out USING btree (is_payout);
 
 
 --
--- TOC entry 1835 (class 2606 OID 20162)
--- Dependencies: 1813 1519 1519
+-- TOC entry 1845 (class 2606 OID 20162)
+-- Dependencies: 1523 1523 1821
 -- Name: blocks_previous_hash_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -332,8 +370,8 @@ ALTER TABLE ONLY blocks
 
 
 --
--- TOC entry 1839 (class 2606 OID 20683)
--- Dependencies: 1525 1813 1519
+-- TOC entry 1849 (class 2606 OID 20683)
+-- Dependencies: 1529 1821 1523
 -- Name: blocks_transactions_block_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -342,8 +380,8 @@ ALTER TABLE ONLY blocks_transactions
 
 
 --
--- TOC entry 1840 (class 2606 OID 20688)
--- Dependencies: 1817 1525 1520
+-- TOC entry 1850 (class 2606 OID 20688)
+-- Dependencies: 1826 1524 1529
 -- Name: blocks_transactions_transaction_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -352,8 +390,8 @@ ALTER TABLE ONLY blocks_transactions
 
 
 --
--- TOC entry 1836 (class 2606 OID 20172)
--- Dependencies: 1521 1520 1817
+-- TOC entry 1846 (class 2606 OID 20172)
+-- Dependencies: 1826 1525 1524
 -- Name: tx_id_transaction_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -362,8 +400,8 @@ ALTER TABLE ONLY tx_in
 
 
 --
--- TOC entry 1837 (class 2606 OID 20177)
--- Dependencies: 1521 1522 1825 1522 1521
+-- TOC entry 1847 (class 2606 OID 20177)
+-- Dependencies: 1526 1835 1525 1525 1526
 -- Name: tx_in_previous_out_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -372,8 +410,8 @@ ALTER TABLE ONLY tx_in
 
 
 --
--- TOC entry 1838 (class 2606 OID 20182)
--- Dependencies: 1522 1817 1520
+-- TOC entry 1848 (class 2606 OID 20182)
+-- Dependencies: 1526 1826 1524
 -- Name: tx_out_transaction_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -381,7 +419,7 @@ ALTER TABLE ONLY tx_out
     ADD CONSTRAINT tx_out_transaction_id_fkey FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id);
 
 
--- Completed on 2011-07-17 01:49:49 CEST
+-- Completed on 2011-07-29 15:40:01 CEST
 
 --
 -- PostgreSQL database dump complete
